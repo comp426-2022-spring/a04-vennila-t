62d0e7e85c691035c1f9ed393a01954f51146692
vennila-t <69809909+vennila-t@users.noreply.github.com>
2022-04-15 12:39:53 -0400

    update

--- database.js
+++ database.js
@@ -7,9 +7,8 @@ const Database = require('better-sqlite3');
 const db = new Database('log.db');
 
 // Is the database initialized or do we need to initialize it?
-const stmt = db.prepare(`
-    SELECT name FROM sqlite_master WHERE type='table' and name='userinfo';`
-    );
+const stmt = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='userinfo';`);
+
 // Define row using `get()` from better-sqlite3
 let row = stmt.get();
 // Check if there is a table. If row is undefined then no table exists.
@@ -18,7 +17,7 @@ if (row === undefined) {
     console.log('Your database appears to be empty. I will initialize it now.');
 // Set a const that will contain your SQL commands to initialize the database.
     const sqlInit = `
-        CREATE TABLE userinfo ( id INTEGER PRIMARY KEY, username TEXT, password TEXT );
+        CREATE TABLE accesslog ( id INTEGER PRIMARY KEY, username TEXT, password TEXT );
         INSERT INTO userinfo (username, password) VALUES ('user1','supersecurepassword'),('test','anotherpassword');
     `;
 // Execute SQL commands that we just wrote above.
--- server.js
+++ server.js
@@ -3,8 +3,36 @@
 // Require Express.js
 const express = require('express')
 const app = express()
+const db = require('/database.js')
+const fs = require('fs')
+const morgan = require('morgan')
 const args = require('yargs').argv
 
+// See what is stored in the object produced by minimist
+console.log(args)
+// Store help text
+const help = (`
+server.js [options]
+
+--port	Set the port number for the server to listen on. Must be an integer
+            between 1 and 65535.
+
+--debug	If set to true, creates endlpoints /app/log/access/ which returns
+            a JSON access log from the database and /app/error which throws
+            an error with the message "Error test successful." Defaults to
+            false.
+
+--log		If set to false, no log files are written. Defaults to true.
+            Logs are always written to database.
+
+--help	Return this message and exit.
+`)
+
+// If --help or -h, echo help text to STDOUT and exit
+if (args.help || args.h) {
+    console.log(help)
+    process.exit(0)
+}
 
 // define port variable
 let port;
@@ -13,6 +41,17 @@ if (typeof args.port === "undefined") {
   port = 5000;
 } else {
   port = args.port;
+}
+
+if(args.log == 'false') {
+  console.log("Not creating a new access.log")
+}
+else {
+  // Use morgan for logging to files
+  // Create a write stream to append (flags: 'a') to a file
+  const WRITESTREAM = fs.createWriteStream('access.log', { flags: 'a' })
+  // Set up the access logging middleware
+  app.use(morgan('combined', { stream: WRITESTREAM }))
 }
 
 // Start an app server
@@ -31,6 +70,35 @@ app.get('/app/', (req, res) => {
     res.end(res.statusCode+ ' ' +res.statusMessage)
 })
 
+// Middleware
+app.use( (req, res, next) => {
+  let logdata = {
+      remoteaddr: req.ip,
+      remoteuser: req.user,
+      time: Date.now(),
+      method: req.method,
+      url: req.url,
+      protocol: req.protocol,
+      httpversion: req.httpVersion,
+      status: res.statusCode,
+      referer: req.headers['referer'],
+      useragent: req.headers['user-agent']
+  }
+  console.log(logdata)
+  const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referrer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
+  const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referrer, logdata.useragent)
+  next();
+  })
+
+if(args.debug === true) {
+  app.get('/app/log/access/', (req, res) => {
+      const stmt = db.prepare("SELECT * FROM accesslog").all()
+      res.status(200).json(stmt)
+  });
+  app.get('/app/error', (req, res) => {
+      throw new Error('Error test successful.')
+  });
+}
 // Endpoint /app/flip/ that returns JSON {"flip":"heads"}
 // or {"flip":"tails"} corresponding to the results of the random coin flip.
 app.get('/app/flip', (req, res) => {
